775a2c4f623cbc1e1467ea8eab84a37bf20002cb
eddieaviles357 <eduardoaviles357@yahoo.com>
2023-04-25 20:00:54 -0700

    class Stack added to use with Tree methods, Tree class method sumValues traverses tree using Depth First Search (DFS) and sums all the values

--- tree.js
+++ tree.js
@@ -15,7 +15,25 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
+    let curr = null;
+    // well use our own stack class here
+    let nodeStack = new Stack();
+    nodeStack.push(this.root);
     
+    let sum = nodeStack.first.val.val;
+
+    while(!nodeStack.isEmpty()) {
+      curr = nodeStack.pop(); // remove node from stack
+
+      if(curr.children.length) { // does node have children?
+        for(let treeNode of curr.children) {
+          nodeStack.push(treeNode); // add children to stack
+          sum+=treeNode.val
+        };
+      };
+    };
+
+    return sum;
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
@@ -31,5 +49,90 @@ class Tree {
 
   }
 }
+let n = new TreeNode(1);
+let n2 = new TreeNode(2);
+let n3 = new TreeNode(3);
+let n4 = new TreeNode(4);
+let n5 = new TreeNode(5);
+let n6 = new TreeNode(6);
+let n7 = new TreeNode(7);
+let n8 = new TreeNode(8);
 
-module.exports = { Tree, TreeNode };
+n.children = [n2, n3, n4];
+
+n4.children.push(n5, n6);
+n6.children.push(n7);
+n7.children.push(n8);
+
+largeTree = new Tree(n);
+
+
+/**
+ ****************************************************************
+ ****************************************************************
+ ******* Stack: chained-together nodes where you can ************
+ ********** remove from the top or add to the top. **************
+ ****************************************************************
+ ****************************************************************
+ */
+ class Node {
+   constructor(val) {
+     this.val = val;
+     this.next = null;
+   }
+ }
+
+class Stack {
+  constructor() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  /** push(val): add new value to end of the stack. Returns undefined. */
+
+  push(val) {
+    // stack is empty first and last are the same value
+    if(this.first == null) {
+      this.first = this.last = new Node(val);
+      this.size++;
+      return;
+    };
+    // add to top of stack
+    let topNode = new Node(val);
+    topNode.next = this.first;
+    this.first = topNode;
+    this.size++;
+    return;
+  };
+
+  /** pop(): remove the node from the top of the stack
+   * and return its value. Should throw an error if the stack is empty. */
+
+  pop() {
+    let popped = null;
+
+    if(this.first === null) throw new Error('Stack is empty!');
+
+    popped = this.first; // to be removed from top of stack
+    this.first = this.first.next; // assign new top of stack
+    popped.next = null; // remove reference to next
+    this.size--; // decrement size
+    return popped.val; // return value of popped node
+  }
+
+  /** peek(): return the value of the first node in the stack. */
+
+  peek() {
+    if(this.first === null) return;
+    return this.first.val;
+  }
+
+  /** isEmpty(): return true if the stack is empty, otherwise false */
+
+  isEmpty() {
+    return (this.first === null) ? true : false;
+  }
+};
+
+// module.exports = { Tree, TreeNode };
